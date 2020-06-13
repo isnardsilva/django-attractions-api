34720d98a7bf61e7101fd03686ebe0f05db2e717
Isnard Silva <isnardsilva@icloud.com>
2020-06-13 18:35:26 -0300

    Create Attractions app

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from attractions.models import Attraction
 
 # Create your models here.
 class TouristSpot(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     okay = models.BooleanField(default=False)
+    attractions = models.ManyToManyField(Attraction)
 
     def __str__(self):
         return self.name
