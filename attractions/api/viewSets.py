6496a46700e81fc1280f26b232b42ab7060d3563
Isnard Silva <isnardsilva@icloud.com>
2020-07-19 02:16:29 -0300

    Add DjangoFiltter Backends

--- attractions/api/viewSets.py
+++ attractions/api/viewSets.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from core.models import Attraction
 from .serializers import AttractionSerializer
@@ -5,3 +6,5 @@ from .serializers import AttractionSerializer
 class AttractionViewSet(ModelViewSet):
     queryset = Attraction.objects.all()
     serializer_class = AttractionSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'description']
--- django_attractions_api/settings.py
+++ django_attractions_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'core',
     'attractions',
     'comments',
@@ -130,3 +131,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = 'images'
 
 MEDIA_URL = '/images/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
