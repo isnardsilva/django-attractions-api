d41380ac1d7d2ae7f7f2a5cd4975b35b9b62efd2
Isnard Silva <isnardsilva@icloud.com>
2020-07-19 00:48:13 -0300

    Create Custom Actions

--- core/api/viewSets.py
+++ core/api/viewSets.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from core.models import TouristSpot
 from .serializers import TouristSpotSerializer
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 class TouristSpotViewSet(ModelViewSet):
     """
@@ -9,32 +10,51 @@ class TouristSpotViewSet(ModelViewSet):
     """
     serializer_class = TouristSpotSerializer
 
+    # ACTIONS
+
     # Executado para fazer a filtragem de resultados
     def get_queryset(self):
         return TouristSpot.objects.filter(okay=True)
 
     # Executado na exibicao da lista de resultados
-    def list(self, request, *args, **kwargs):
-        return Response({'teste': 123})
+    # def list(self, request, *args, **kwargs):
+    #     return Response({'teste': 123})
 
     # Executando quando um recurso vai ser criado
-    def create(self, request, *args, **kwargs):
-        return Response({'Hello': request.data['name']})
+    # def create(self, request, *args, **kwargs):
+    #     return Response({'Hello': request.data['name']})
 
     # Executado quando um recurso vai ser deletado
-    def destroy(self, request, *args, **kwargs):
-        pass
+    # def destroy(self, request, *args, **kwargs):
+    #     pass
 
     # Executado, assim como o LIST, quando o usuario pede para ver os
     # dados dos recursos, mas esse metodo e executado quando
     # quando eu quero obter um recurso em especifico e nao
     # a lista de todos os recursos presentes no banco
-    def retrieve(self, request, *args, **kwargs):
+    # def retrieve(self, request, *args, **kwargs):
+    #     pass
+
+    # Executado quando um recurso vai ser atualizado por inteiro (PUT)
+    # def update(self, request, *args, **kwargs):
+    #     pass
+
+    # Executado quando apenas parte de um recurso vai ser atualizado (PATCH)
+    # def partial_update(self, request, *args, **kwargs):
+    #     pass
+
+
+    # CUSTOM ACTIONS
+
+    # Custom Action que trabalhar com um recurso especifico
+    # @action(methods=['get'], detail=True)
+    @action(methods=['post', 'get'], detail=True)
+    def denounce(self, request, pk=None):
+        return Response({'denounce': pk})
         pass
 
-    # Executado quando um recurso vai ser atualizado por inteiro
-    def update(self, request, *args, **kwargs):
-        pass
-
-    def partial_update(self, request, *args, **kwargs):
-        pass
+    
+    # Custom Action que trabalha com um conjunto de recursos
+    @action(methods=['get'], detail=False)
+    def testAllList(self, request):
+        return Response({'testAllList': 'sucess'})
