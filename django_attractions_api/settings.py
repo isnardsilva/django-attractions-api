76b3ba455f78bca457642718026894a489adeb05
Isnard Silva <isnardsilva@icloud.com>
2020-07-21 09:49:25 -0300

    Add Token Auth

--- django_attractions_api/settings.py
+++ django_attractions_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'django_filters',
     'core',
     'attractions',
@@ -132,6 +133,6 @@ MEDIA_ROOT = 'images'
 
 MEDIA_URL = '/images/'
 
-REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+# }
--- django_attractions_api/urls.py
+++ django_attractions_api/urls.py
@@ -24,6 +24,7 @@ from attractions.api.viewsets import AttractionViewSet
 from addresses.api.viewsets import AddressViewSet
 from comments.api.viewsets import CommentViewSet
 from reviews.api.viewsets import ReviewViewSet
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'touristspot', TouristSpotViewSet, basename='TouristSpot')
@@ -35,4 +36,5 @@ router.register(r'reviews', ReviewViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', views.obtain_auth_token),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
